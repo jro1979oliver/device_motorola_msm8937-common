aeffaf0a40df7d40962ed6874765d57baf9ba044
Mathieu Chartier <mathieuc@google.com>
2018-06-26 00:14:38 +0200

    msm8937-common: Always preopt extracted APKs
    Change-Id: I2e4742a796b8142f14eaad5eba81eaf800d64914

--- msm8937.mk
+++ msm8937.mk
@@ -405,3 +405,7 @@ PRODUCT_PACKAGES += com.android.tethering.inprocess
 PRODUCT_USE_PROFILE_FOR_BOOT_IMAGE := true
 PRODUCT_DEX_PREOPT_BOOT_IMAGE_PROFILE_LOCATION := frameworks/base/config/boot-image-profile.txt
 
+# Always preopt extracted APKs to prevent extracting out of the APK
+# for gms modules.
+PRODUCT_ALWAYS_PREOPT_EXTRACTED_APK := true
+
