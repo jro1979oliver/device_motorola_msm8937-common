96b506a1922359379108375bd374779c15b1fdea
Jarl-Penguin <jarlpenguin@outlook.com>
2020-10-02 10:28:58 +0300

    msm8937-common: Cleanup audio HIDL packages
    Signed-off-by: Jarl-Penguin <jarlpenguin@outlook.com>
    Change-Id: Idebfc72cfb6499584dc7404f4b36a6ffc2c56221

--- msm8937.mk
+++ msm8937.mk
@@ -43,18 +43,11 @@ PRODUCT_PACKAGES += \
     libqcomvoiceprocessing
 
 PRODUCT_PACKAGES += \
-    android.hardware.audio@2.0-impl \
     android.hardware.audio@2.0-service \
-    android.hardware.audio@4.0 \
     android.hardware.audio@6.0-impl \
-    android.hardware.audio.common@4.0 \
-    android.hardware.audio.common@4.0-util \
-    android.hardware.audio.effect@2.0-impl \
-    android.hardware.audio.effect@4.0 \
     android.hardware.audio.effect@6.0-impl \
     android.hardware.bluetooth.audio@2.0-impl \
     android.hardware.soundtrigger@2.2-impl \
-    android.hardware.soundtrigger@2.2-service \
     vendor.qti.hardware.btconfigstore@1.0.vendor
 
 PRODUCT_COPY_FILES += \
