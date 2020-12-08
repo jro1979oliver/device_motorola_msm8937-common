8add4e00c32f64179d34fa542508613c17cec99b
jro1979oliver <jroliveira_oliveira301@gmail.com>
2020-12-08 18:57:12 +0100

    msm8937-common: Import relevant Android GO configs
    Change-Id: Ie6dc00094c6af8f561a5285daf74273a3e7d621d

--- msm8937.mk
+++ msm8937.mk
@@ -400,3 +400,8 @@ PRODUCT_BOOT_JARS += \
 PRODUCT_PACKAGES += InProcessNetworkStack
 PRODUCT_PACKAGES += com.android.tethering.inprocess
 
+# Use a profile based boot image for this device. Note that this is currently a
+# generic profile and not Android Go optimized.
+PRODUCT_USE_PROFILE_FOR_BOOT_IMAGE := true
+PRODUCT_DEX_PREOPT_BOOT_IMAGE_PROFILE_LOCATION := frameworks/base/config/boot-image-profile.txt
+
